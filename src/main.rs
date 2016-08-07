a68c11c511a93853d5fb9ba5cf645dd298a2513b
isaev-bolsen <al_mamistov@mail.ru>
2016-08-07 11:03:01 +0300

    Array of arrays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+enum Cell
+{
+    E,
+    X,
+    O,
+}
+
+impl Copy for Cell {}
+impl Clone for Cell {
+     fn clone(&self) -> Cell { *self } 
+}
+
+
 fn main() {
+
+    let mut field=[[Cell::E;3];3];
+    field[1][1]=Cell::X;
+    field[1][2]=Cell::O;
+
     println!("Hello, world!");
 }
